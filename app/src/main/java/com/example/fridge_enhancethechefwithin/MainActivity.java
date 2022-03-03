d0d798b29df46bdc62a8b24263a660d68444ec84
Abdul Aziz Farid <F18CSC11@shu.edu.pk>
2022-03-03 16:44:33 +0500

    SQLite DB updated

--- app/src/main/java/com/example/fridge_enhancethechefwithin/MainActivity.java
+++ app/src/main/java/com/example/fridge_enhancethechefwithin/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import net.sourceforge.jtds.jdbc.Driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -27,20 +26,22 @@ import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
    /* public static String ip = "192.168.18.36";
-    public static String port = "1433";
     //public static String classes = "com.mysql.jdbc.Driver";
-    public static String classes = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     //public static String database = "TestDB1"; //For MySQL
     //ublic static String username = "root";
     //blic static String password = "";
-    public static String database = "testdb"; //For MS SQL
-    public static String username = "test";
-    public static String password = "test";
    // public static String url = "jdbc:mysql://"+ip+":"+port+"/"+database;
-    public static String url = "jdbc:jtds:sqlserver://"+ip+":"+port+"/"+database;
     //public static String urlAzure = "jdbc:sqlserver://lifeless.database.windows.net:1433;database=Test1;user=admintest@lifeless;password=Six170528p;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
     public static Connection connection = null; */
-
+    public static String database = "db_a83a8a_fridge"; //For MS SQL
+    public static String username = "db_a83a8a_fridge_admin";
+    public static String password = "fridge123";
+    public static String classes = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String ip = "SQL5107.site4now.net";
+    public static String port = "1433";
+    //public static String url = "jdbc:jtds:sqlserver://"+ip+":"+port+";"+"databasename="+database+"; user="+username+"; password="+password+";";
+    public static String url = String.format("jdbc:sqlserver://%s:%s;databaseName=%s;user=%s;password=%s",ip, port, database, username, password);
+    public static Connection connection = null;
     public CardView CardHome;
 
     TextView textView;
@@ -53,17 +54,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         CardHome = (CardView) findViewById(R.id.HomeCard);
 
-        //textView = findViewById(R.id.txtTitle);
-        /*
+        textView = findViewById(R.id.txtTitle);
+
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
         try {
-            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Class.forName(classes);
-            //connection = DriverManager.getConnection(urlAzure, "admintest@lifeless", "Six170528p");
-            connection = DriverManager.getConnection(url, username, password);
+            //Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //Class.forName(classes);
+            //connection = DriverManager.getConnection(urlAzure, "admintest@lifeless", "PlSw0rk1");
+            connection = DriverManager.getConnection(url);
+            // connection = DriverManager.getConnection(url, username, password);
             textView.setText("Successfully Connected");
         } catch (ClassNotFoundException e) {
             textView.setText("Class error");
@@ -72,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             textView.setText("SQL error");
             e.printStackTrace();
         }
-        */
+
     }
     public void login(View v){
         Intent i;
