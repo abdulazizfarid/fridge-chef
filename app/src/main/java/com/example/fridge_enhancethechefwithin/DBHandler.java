3a23c6c3afd5661b68a2e1d2a52fe9febaabf9f2
CS Lab3  Computer Science Lab3 <cslab3@shu.edu.pk>
2022-03-04 10:54:43 +0500

    Some more changes

---
+++ app/src/main/java/com/example/fridge_enhancethechefwithin/DBHandler.java
@@ -0,0 +1,230 @@
+package com.example.fridge_enhancethechefwithin;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.PreparedStatement;
+
+public class DBHandler extends SQLiteOpenHelper{
+    public static final int dbVersion = 1;
+    public static final String dbPath = "/data/data/com.example.fridge_enhancethechefwithin/databases/";
+    public static final String dbName ="Fridge";
+    SQLiteDatabase myDatabase;
+    private final Context mContext;
+
+    public DBHandler(Context context) {
+        super(context, dbName, null, dbVersion );
+        this.mContext = context;
+        try {
+            createDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void copyDatabase() throws IOException {
+
+        //Open your local db as the input stream
+        InputStream myInput = mContext.getAssets().open(dbName);// + ".db");
+
+        // Path to the just created empty db
+        String outFileName = dbPath + dbName;
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
+    }
+
+    public void createDatabase() throws IOException{
+        boolean exists = checkDatabase();
+        if(!exists) {
+            try {
+                this.getReadableDatabase();
+                this.close();
+                copyDatabase();
+            } catch (Exception exc){
+                exc.printStackTrace();
+            } /*catch (IOException mIOException){
+                mIOException.printStackTrace();
+                throw new Error("Error copying DB");
+            } */finally {
+                this.close();
+            }
+        }
+    }
+
+    @Override
+    public synchronized void close(){
+        if(myDatabase != null)
+            myDatabase.close();
+        SQLiteDatabase.releaseMemory();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {}
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {}
+
+    private boolean checkDatabase(){
+        try{
+            final String mPath = dbPath + dbName;
+            final File file = new File(mPath);
+            if (file.exists()) return true;
+            else return false;
+        } catch (SQLiteException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Cursor getID(){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        //Cursor cursor = DB.rawQuery("SELECT ingredient_id FROM ingredient where name IN ('" + Home.Selected.get(0) + "','" + Home.Selected.get(1) + "');" ,null);
+        int n = Home.Selected.size();
+        Cursor cursor=null;
+        if (!Home.Selected.isEmpty()) {
+            String query = "SELECT ingredient_id FROM ingredients where name IN (";
+            for (int i = 0; i < n; i++) {
+                if (i < (n - 1)) {
+                    query = query + "'" + Home.Selected.get(i) + "',";
+                } else if (i == (n - 1)) {
+                    query = query + "'" + Home.Selected.get(i) + "');";
+                }
+            }
+            cursor = DB.rawQuery(query,null);
+            return cursor;
+        }
+        //query = query + "SELECT ingredient_id FROM ingredient where name IN (" + query + ");";
+        cursor = DB.rawQuery("SELECT name from ingredients where ingredient_ID='999'",null);
+        return cursor;
+    }
+
+    public Cursor getRecipes(){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        int n = Home.SelectedID.size();
+        Cursor cursor=null;
+        if (!Home.SelectedID.isEmpty()) {
+            DB.execSQL("DROP VIEW IF EXISTS T1;");
+            DB.execSQL("DROP VIEW IF EXISTS T2;");
+            DB.execSQL("DROP VIEW IF EXISTS T9119;");
+            String viewT99 = "CREATE VIEW T9119 AS SELECT * FROM Recipes";
+            String viewT1 = //"DROP VIEW IF EXISTS T1;\n" +
+                    "CREATE VIEW T1\n" +
+                    "AS\n" +
+                    "select  rec_id, count(*) as NumIngrd \n" +
+                    "from recipe_ingredient\n" +
+                    "group by rec_id;";
+            String viewT2 =// "DROP VIEW IF EXISTS T2;\n" +
+                    "CREATE VIEW T2\n" +
+                    "As \n" +
+                    "select count(*) as NumMatches , r.recipe_id, r.ingredients, r.recipe_name, r.instructions\n" +
+                    "from \n" +
+                    "    Recipes r\n" +
+                    "    inner join recipe_ingredient ri\n" +
+                    "    on ri.rec_id = r.recipe_id\n" +
+                    "where ri.ing_id IN (";
+            for (int i = 0; i < n; i++) {
+                if (i < (n - 1)) {
+                    viewT2 = viewT2 + "'" + Home.SelectedID.get(i) + "',";
+                } else if (i == (n - 1)) {
+                    viewT2 = viewT2 + "'" + Home.SelectedID.get(i) + "')\n";
+                }
+            }
+            viewT2 = viewT2 + "group by r.recipe_id;";
+
+            DB.execSQL(viewT99);
+            //DB.rawQuery("DROP VIEW IF EXISTS T1;",null);
+            //DB.rawQuery(viewT1,null);
+            //DB.rawQuery(viewT2,null);
+            DB.execSQL(viewT1);
+            DB.execSQL(viewT2);
+            //DB.rawQuery(viewT99,null);
+            String query = "SELECT recipe_name, instructions, ingredients FROM\n" +
+                    "T1 INNER JOIN T2\n" +
+                    "ON T1.rec_id = T2.recipe_id\n" +
+                    "WHERE NumMatches=NumIngrd;";
+            //String query1="SELECT recipe_name, instructions, ingredients FROM T2";
+            //cursor = DB.rawQuery(query1,null);
+            cursor = DB.rawQuery(query,null);
+            //cursor = DB.rawQuery("SELECT name from ingredients where ingredient_ID='999'",null);
+            return cursor;
+        }
+        cursor = DB.rawQuery("SELECT name from ingredients where ingredient_ID='999'",null);
+
+        return cursor;
+    }
+
+    public Cursor getAuth(String email){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        try{
+            Cursor cursor = DB.rawQuery("SELECT * FROM Users WHERE email = '" + email + "'", null);
+            return cursor;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void signup(String email, String password){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        try{
+            DB.execSQL("INSERT INTO Users (email, password) VALUES (?,?)",new Object[]{email, password});
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Cursor getFavorites(String email){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("SELECT recipe_name, ingredients, instructions FROM Recipes WHERE recipe_id IN (SELECT recipeId FROM UserFavorites WHERE userEmail = ?)", new String[]{email});
+        return cursor;
+    }
+
+    public Cursor getFavId(String email){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("SELECT recipeId FROM UserFavorites WHERE userEmail = '" + email + "'", null);
+        return cursor;
+    }
+
+    public void setFav(String email, int recipeId){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        try{
+            DB.execSQL("INSERT INTO UserFavorites (userEmail, recipeId) VALUES (?,?)",new Object[]{email, recipeId});
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void removeFav(String email, int recipeId){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        try{
+            //DB.execSQL("DELETE FROM UserFavorites WHERE userEmail = ? AND recipeId = ?",new Object[]{email, recipeId});
+            DB.delete("UserFavorites", "userEmail = '" + email + "' AND recipeId = '" + recipeId + "'", null);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fridge_enhancethechefwithin/MainActivity.java
@@ -0,0 +1,233 @@
+package com.example.fridge_enhancethechefwithin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class MainActivity extends AppCompatActivity {
+   /* public static String ip = "192.168.18.36";
+    //public static String classes = "com.mysql.jdbc.Driver";
+    //public static String database = "TestDB1"; //For MySQL
+    //ublic static String username = "root";
+    //blic static String password = "";
+   // public static String url = "jdbc:mysql://"+ip+":"+port+"/"+database;
+    //public static String urlAzure = "jdbc:sqlserver://lifeless.database.windows.net:1433;database=Test1;user=admintest@lifeless;password=Six170528p;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+    public static Connection connection = null; */
+
+    /*
+    public static String database = "db_a83a8a_fridge"; //For MS SQL
+    public static String username = "db_a83a8a_fridge_admin";
+    public static String password = "fridge123";
+    public static String classes = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String ip = "SQL5107.site4now.net";
+    public static String port = "1433";
+    public static String protocol = "TLS";
+    public static String url = String.format("jdbc:sqlserver://%s:%s;databaseName=%s;user=%s;password=%s;sslProtocol=TLSv1.2",ip, port, database, username, password);
+    public static Connection connection = null; */
+    //public static String url = "jdbc:jtds:sqlserver://"+ip+":"+port+";"+"databasename="+database+"; user="+username+"; password="+password+";";
+    ArrayList<String> list;
+    DBHandler DB;
+    boolean auth = false;
+    Cursor usr;
+    String mode = "";
+    public static ArrayList<String> RecipeNames = new ArrayList<>();
+    public static ArrayList<String> RecipeInstructions = new ArrayList<>();
+    public static ArrayList<String> RecipeIngredients = new ArrayList<>();
+    public CardView CardHome;
+    EditText etEmail;
+    EditText etPassword;
+    public TextView txtHome;
+    CardView logoutCard;
+    CardView favCard;
+    SharedPreferences sharedPrefs;
+    SharedPreferences.Editor spEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        DB = new DBHandler(this);
+        CardHome = (CardView) findViewById(R.id.HomeCard);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        txtHome = (TextView) findViewById(R.id.txtHome);
+        logoutCard = (CardView) findViewById(R.id.logoutCard);
+        favCard = (CardView) findViewById(R.id.favCard);
+        sharedPrefs = getSharedPreferences("file", MODE_PRIVATE);
+        spEdit = sharedPrefs.edit();
+
+        boolean valueFromSharedPrefs = sharedPrefs.getBoolean("isUserLoggedIn", false);
+        if (valueFromSharedPrefs) {
+            logoutCard.setVisibility(View.VISIBLE);
+            favCard.setVisibility(View.VISIBLE);
+            Toast.makeText(this,"Welcome back!", Toast.LENGTH_SHORT).show();
+            etEmail.setVisibility(View.INVISIBLE);
+            etPassword.setVisibility(View.INVISIBLE);
+        }
+
+        etEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    //Toast.makeText(getApplicationContext(), "Lost the focus", Toast.LENGTH_SHORT).show();
+                    String email = etEmail.getText().toString();
+                    if (email != null && email.contains("@"))
+                        usr = DB.getAuth(email);
+                    else if(email == null)
+                        Toast.makeText(getBaseContext(),"Email can not be empty", Toast.LENGTH_SHORT).show();
+                    else if(!email.contains("@"))
+                        Toast.makeText(getBaseContext(),"Please enter a valid email", Toast.LENGTH_SHORT).show();
+
+                    if (usr.getCount()!=0){
+                        etPassword.setHint("Enter your password");
+                        etPassword.setVisibility(View.VISIBLE);
+                        txtHome.setText("Login");
+                        mode = "login";
+                    }
+                    else{
+                        etPassword.setHint("Enter new password");
+                        etPassword.setVisibility(View.VISIBLE);
+                        txtHome.setText("Signup");
+                        mode = "signup";
+                    }
+
+                }
+            }
+        });
+        /*textView = findViewById(R.id.txtTitle);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);*/
+
+        /*try {
+            //Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //Class.forName(classes).newInstance();
+            //connection = DriverManager.getConnection(urlAzure, "admintest@lifeless", "PlSw0rk1");
+            //--Main-- // connection = DriverManager.getConnection(url);
+            // connection = DriverManager.getConnection(url, username, password);
+            textView.setText("Successfully Connected");
+        } *//*catch (ClassNotFoundException e) {
+            textView.setText("Class error");
+            e.printStackTrace();
+        }*//* catch (SQLException e) {
+            textView.setText("SQL error");
+            e.printStackTrace();
+        }*/
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Toast.makeText(this, "Resumed", Toast.LENGTH_SHORT).show();
+        boolean valueFromSharedPrefs = sharedPrefs.getBoolean("isUserLoggedIn", false);
+        if (valueFromSharedPrefs) {
+            logoutCard.setVisibility(View.VISIBLE);
+            favCard.setVisibility(View.VISIBLE);
+            etEmail.setVisibility(View.INVISIBLE);
+            etPassword.setVisibility(View.INVISIBLE);
+        }
+        txtHome.setText("Let's Get Started");
+    }
+
+    public void logout(View v) {
+        spEdit.putBoolean("isUserLoggedIn", false);
+        spEdit.putString("userEmail", "");
+        spEdit.apply();
+        Toast.makeText(getBaseContext(),"Logged out!", Toast.LENGTH_SHORT).show();
+        etEmail.setVisibility(View.VISIBLE);
+
+        //etPassword.setVisibility(View.VISIBLE);
+        logoutCard.setVisibility(View.INVISIBLE);
+        favCard.setVisibility(View.INVISIBLE);
+    }
+
+    public void getFavs(View v) {
+        Cursor res = DB.getFavorites(sharedPrefs.getString("userEmail", "x"));
+        while (res.moveToNext()){
+            RecipeNames.add(res.getString(0));
+            RecipeInstructions.add(res.getString(1));
+            RecipeIngredients.add(res.getString(2));
+        }
+        Intent i = new Intent(getBaseContext(), Results.class);
+        i.putExtra("mode", "favorite");
+        startActivity(i);
+    }
+
+    public void next(View v){
+
+        SharedPreferences sharedPrefs = getSharedPreferences("file", MODE_PRIVATE);
+        SharedPreferences.Editor spEdit = sharedPrefs.edit();
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        if (mode.equals("login")){
+            usr.moveToFirst();
+            if (etPassword.getText().toString() != null && usr.getString(1).equals(etPassword.getText().toString()))
+                auth = true;
+            else
+                Toast.makeText(getBaseContext(),"Invalid Email/Password", Toast.LENGTH_SHORT).show();
+        }
+        else if (mode.equals("signup")){
+            DB.signup(etEmail.getText().toString(), etPassword.getText().toString());
+            auth = true;
+        }
+
+        if(auth){
+            spEdit.putString("userEmail", etEmail.getText().toString());
+            spEdit.putBoolean("isUserLoggedIn", true);
+            spEdit.apply();
+            Intent i = new Intent(getBaseContext(), Home.class);
+            startActivity(i);
+
+        }
+    }
+
+    //Copy pasted code to remove focus on touch outside EditText
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    Log.d("focus", "touchevent");
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
+}
